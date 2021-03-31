243fb527cb14e0a7fd7e580bcbdc2977c7acd5de
ShivamKumarJha <jha.shivam3@gmail.com>
2021-03-31 20:19:24 +0530

    Gocdn Api: Use resource for status
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/supaflix/repository/GocdnCloudRepository.kt
+++ app/src/main/java/com/shivamkumarjha/supaflix/repository/GocdnCloudRepository.kt
@@ -1,8 +1,9 @@
 package com.shivamkumarjha.supaflix.repository
 
 import com.shivamkumarjha.supaflix.model.gocdn.Gocdn
+import com.shivamkumarjha.supaflix.network.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface GocdnCloudRepository {
-    suspend fun getLink(video_hash: String): Flow<Gocdn?>
+    suspend fun getLink(video_hash: String): Flow<Resource<Gocdn?>>
 }
--- app/src/main/java/com/shivamkumarjha/supaflix/repository/GocdnCloudRepositoryImpl.kt
+++ app/src/main/java/com/shivamkumarjha/supaflix/repository/GocdnCloudRepositoryImpl.kt
@@ -4,21 +4,32 @@ import android.util.Log
 import com.shivamkumarjha.supaflix.config.Constants
 import com.shivamkumarjha.supaflix.model.gocdn.Gocdn
 import com.shivamkumarjha.supaflix.network.ApiGocdnCloud
+import com.shivamkumarjha.supaflix.network.NoConnectivityException
+import com.shivamkumarjha.supaflix.network.Resource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
 class GocdnCloudRepositoryImpl(private val apiGocdnCloud: ApiGocdnCloud) : GocdnCloudRepository {
-    override suspend fun getLink(video_hash: String): Flow<Gocdn?> = flow {
+    override suspend fun getLink(video_hash: String): Flow<Resource<Gocdn?>> = flow {
+        emit(Resource.loading(data = null))
         try {
             val response = apiGocdnCloud.getLink(video_hash)
             if (response.isSuccessful) {
-                emit(response.body())
+                emit(Resource.success(data = response.body()))
                 Log.d(Constants.TAG, response.body().toString())
+            } else {
+                emit(Resource.error(data = null, message = response.code().toString()))
+                Log.d(Constants.TAG, response.code().toString())
             }
         } catch (exception: Exception) {
-            Log.d(Constants.TAG, exception.message.toString())
+            if (exception is NoConnectivityException)
+                emit(Resource.offline(data = null))
+            else {
+                emit(Resource.error(data = null, message = exception.message.toString()))
+                Log.e(Constants.TAG, exception.message.toString())
+            }
         }
     }.flowOn(Dispatchers.IO)
 }
