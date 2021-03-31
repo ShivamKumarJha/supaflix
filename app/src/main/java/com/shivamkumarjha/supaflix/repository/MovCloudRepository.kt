d20c1e41aff46cae8b12568b1710962bd35763df
ShivamKumarJha <jha.shivam3@gmail.com>
2021-03-31 20:20:26 +0530

    Movcloud Api: Use resource for status
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/supaflix/repository/MovCloudRepository.kt
+++ app/src/main/java/com/shivamkumarjha/supaflix/repository/MovCloudRepository.kt
@@ -1,8 +1,9 @@
 package com.shivamkumarjha.supaflix.repository
 
 import com.shivamkumarjha.supaflix.model.movcloud.MovCloud
+import com.shivamkumarjha.supaflix.network.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface MovCloudRepository {
-    suspend fun getLink(hash: String): Flow<MovCloud?>
+    suspend fun getLink(hash: String): Flow<Resource<MovCloud?>>
 }
--- app/src/main/java/com/shivamkumarjha/supaflix/repository/MovCloudRepositoryImpl.kt
+++ app/src/main/java/com/shivamkumarjha/supaflix/repository/MovCloudRepositoryImpl.kt
@@ -4,21 +4,32 @@ import android.util.Log
 import com.shivamkumarjha.supaflix.config.Constants
 import com.shivamkumarjha.supaflix.model.movcloud.MovCloud
 import com.shivamkumarjha.supaflix.network.ApiMovCloud
+import com.shivamkumarjha.supaflix.network.NoConnectivityException
+import com.shivamkumarjha.supaflix.network.Resource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
 class MovCloudRepositoryImpl(private val apiMovCloud: ApiMovCloud) : MovCloudRepository {
-    override suspend fun getLink(hash: String): Flow<MovCloud?> = flow {
+    override suspend fun getLink(hash: String): Flow<Resource<MovCloud?>> = flow {
+        emit(Resource.loading(data = null))
         try {
             val response = apiMovCloud.getLink(hash)
             if (response.isSuccessful) {
-                emit(response.body())
+                emit(Resource.success(data = response.body()))
                 Log.d(Constants.TAG, response.body().toString())
+            } else {
+                emit(Resource.error(data = null, message = response.code().toString()))
+                Log.d(Constants.TAG, response.code().toString())
             }
         } catch (exception: Exception) {
-            Log.d(Constants.TAG, exception.message.toString())
+            if (exception is NoConnectivityException)
+                emit(Resource.offline(data = null))
+            else {
+                emit(Resource.error(data = null, message = exception.message.toString()))
+                Log.e(Constants.TAG, exception.message.toString())
+            }
         }
     }.flowOn(Dispatchers.IO)
 }
