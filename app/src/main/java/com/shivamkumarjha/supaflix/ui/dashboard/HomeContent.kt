efb49d72037478738f2f743ef572fd6b0ba72174
ShivamKumarJha <jha.shivam3@gmail.com>
2021-04-01 20:26:49 +0530

    Add contents screen
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/supaflix/ui/dashboard/HomeContent.kt
+++ app/src/main/java/com/shivamkumarjha/supaflix/ui/dashboard/HomeContent.kt
@@ -17,9 +17,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.shivamkumarjha.supaflix.R
+import com.shivamkumarjha.supaflix.config.Constants
+import com.shivamkumarjha.supaflix.model.xmovies.Contents
 import com.shivamkumarjha.supaflix.model.xmovies.Covers
 import com.shivamkumarjha.supaflix.network.Resource
 import com.shivamkumarjha.supaflix.ui.theme.ColorUtility
@@ -33,17 +37,67 @@ fun HomeContent(navController: NavController, viewModel: DashboardViewModel) {
             .fillMaxSize()
             .background(ColorUtility.surfaceBackground(isSystemInDarkTheme()))
     ) {
-
         val home = viewModel.home.observeAsState(Resource.loading(null))
         if (home.value.data != null) {
+            ContentsRow(
+                heading = stringResource(id = R.string.movies),
+                contents = home.value.data?.movies ?: listOf()
+            )
+            ContentsRow(
+                heading = stringResource(id = R.string.series),
+                contents = home.value.data?.series ?: listOf()
+            )
             CoversRow(covers = home.value.data?.covers ?: listOf())
-            ListItemDivider()
         }
     }
 }
 
 @Composable
+fun ContentsRow(heading: String, contents: List<Contents>) {
+    val modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 8.dp, top = 8.dp)
+    Text(
+        text = heading,
+        style = typography.h6,
+        color = ColorUtility.textColor(isSystemInDarkTheme()),
+        modifier = modifier
+    )
+    LazyRow {
+        items(contents) { content ->
+            Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = modifier) {
+                CoilImage(
+                    data = Constants.XMOVIES8_STATIC_URL + content.poster_path,
+                    contentDescription = content.name,
+                    loading = {
+                        Box(Modifier.matchParentSize()) {
+                            CircularProgressIndicator(Modifier.align(Alignment.Center))
+                        }
+                    },
+                    modifier = Modifier
+                        .height(225.dp)
+                        .width(150.dp)
+                        .clip(RoundedCornerShape(12.dp)),
+                    contentScale = ContentScale.Fit
+                )
+                Text(
+                    text = "\t" + content.name + "\t",
+                    color = ColorUtility.textColor(isSystemInDarkTheme()),
+                    style = typography.body2,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+    ListItemDivider()
+}
+
+@Composable
 fun CoversRow(covers: List<Covers>) {
+    Text(
+        text = stringResource(id = R.string.top_picks),
+        style = typography.h6,
+        color = ColorUtility.textColor(isSystemInDarkTheme()),
+        modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 8.dp, top = 8.dp)
+    )
     LazyRow {
         items(covers) { cover ->
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
@@ -63,12 +117,14 @@ fun CoversRow(covers: List<Covers>) {
                 )
                 Text(
                     text = "\t" + cover.name + "\t",
+                    color = ColorUtility.textColor(isSystemInDarkTheme()),
                     style = typography.body2,
                     textAlign = TextAlign.Center
                 )
             }
         }
     }
+    ListItemDivider()
 }
 
 @Composable
