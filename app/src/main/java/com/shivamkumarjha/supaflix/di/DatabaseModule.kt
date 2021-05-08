677c44870159d8a7c49c4f9f8b8baa1e6c504950
ShivamKumarJha <jha.shivam3@gmail.com>
2021-05-08 22:22:40 +0530

    Add Room database migration
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/supaflix/di/DatabaseModule.kt
+++ app/src/main/java/com/shivamkumarjha/supaflix/di/DatabaseModule.kt
@@ -2,6 +2,8 @@ package com.shivamkumarjha.supaflix.di
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.shivamkumarjha.supaflix.config.Constants
 import com.shivamkumarjha.supaflix.persistence.SupaflixDatabase
 import dagger.Module
@@ -15,10 +17,17 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 class DatabaseModule {
 
+    private val MIGRATION_1_2 = object : Migration(1, 2) {
+        override fun migrate(database: SupportSQLiteDatabase) {
+            database.execSQL("CREATE TABLE IF NOT EXISTS `download` (`downloadID` INTEGER NOT NULL, `url` TEXT NOT NULL, `type` TEXT NOT NULL, `history` TEXT NOT NULL, `filePath` TEXT, PRIMARY KEY(`downloadID`))")
+        }
+    }
+
     @Provides
     @Singleton
     fun supaflixDatabase(@ApplicationContext context: Context): SupaflixDatabase =
         Room.databaseBuilder(context, SupaflixDatabase::class.java, Constants.DB_SUPAFLIX)
+            .addMigrations(MIGRATION_1_2)
             .fallbackToDestructiveMigration()
             .build()
 
