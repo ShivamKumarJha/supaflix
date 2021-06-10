25c5e0d2106268fbb5edf398db07f17543f7ca32
ShivamKumarJha <jha.shivam3@gmail.com>
2021-06-11 00:03:14 +0530

    Add FbFlipper leak canary plugin
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- app/src/main/java/com/shivamkumarjha/supaflix/ui/BaseApplication.kt
+++ app/src/main/java/com/shivamkumarjha/supaflix/ui/BaseApplication.kt
@@ -5,10 +5,13 @@ import com.facebook.flipper.android.AndroidFlipperClient
 import com.facebook.flipper.android.utils.FlipperUtils
 import com.facebook.flipper.plugins.inspector.DescriptorMapping
 import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.leakcanary2.FlipperLeakListener
+import com.facebook.flipper.plugins.leakcanary2.LeakCanary2FlipperPlugin
 import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
 import com.facebook.soloader.SoLoader
 import com.shivamkumarjha.supaflix.BuildConfig
 import dagger.hilt.android.HiltAndroidApp
+import leakcanary.LeakCanary
 import javax.inject.Inject
 
 
@@ -21,12 +24,18 @@ class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        //Set the flipper listener in leak canary config
+        LeakCanary.config = LeakCanary.config.copy(
+            onHeapAnalyzedListener = FlipperLeakListener()
+        )
+
         //FbFlipper
         SoLoader.init(this, false)
         if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
             val client = AndroidFlipperClient.getInstance(this)
             client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
             client.addPlugin(networkFlipperPlugin)
+            client.addPlugin(LeakCanary2FlipperPlugin())
             client.start()
         }
     }
